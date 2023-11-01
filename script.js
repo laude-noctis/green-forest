70c7ad53dcee86bc52f998da15994852d3f67f0f
unknown <lizzybumpers11@gmail.com>
2023-11-01 11:58:56 -0500

    commiting where I am at

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let displayCityName = document.getElementById("display-city");
-let currentDay = dayjs().format("MM/DD/YY");
+let currentDate = dayjs().format("MM/DD/YYYY");
 let currentTemp = document.getElementById("currentTemp")
 let currentWind = document.getElementById("currentWind");
 let currentHumidity = document.getElementById("currentHumidity");
@@ -8,24 +8,25 @@ let pear = (3 * 3)
 let orange = "f1000ea571f235be1"
 const lat = 40.73; // new york
 const lon = 73.93; 
-const apiKey = apple + pear + orange;
 
-fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`)
+
+fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apple + pear + orange}&units=imperial`)
   .then(response => response.json())
   .then(data => {
     let weatherData = data;
     let weatherTemp = data.main.temp;
     let weatherHumidity = data.main.humidity
-    let weatherWind = data.wind;
+    let weatherWind = data.wind.speed;
 
     currentTemp.innerHTML = "Temp: " + weatherTemp + "℉";
-    currentWind.innerHTML = "Wind: " + weatherWind;
-    currentHumidity.innerHTML = "Humidity: " + weatherHumidity
+    currentWind.innerHTML = "Wind: " + weatherWind + " MPH";
+    currentHumidity.innerHTML = "Humidity: " + weatherHumidity + "%"
 
     console.log(data)
     console.log(data.main.temp);
     console.log(data.main.humidity);
     console.log(data.wind);
+
   })
   .catch(error => {
     console.error(error);
@@ -34,5 +35,8 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&app
 function getCity() {
     let input = document.getElementById("citysearch")
     let citySearch = input.value
-    displayCityName.innerHTML = citySearch + " " + currentDay
+    displayCityName.innerHTML = citySearch + " " + currentDate
 }
+
+let forecastDate = dayjs().add(1, "day");
+console.log(forecastDate.format("MM/DD/YY"))
